6a5561451ab7b4ee942014fdeb690763de3d9b47
imeldaputr <imel.kein.email@gmail.com>
2025-08-06 16:58:10 +0200

    Add initial migration and models for Meetup, update templates and URLs

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
