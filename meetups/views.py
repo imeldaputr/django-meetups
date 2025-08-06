783fd36ec9276d9e2b4885a38c7787bef1c28b16
imeldaputr <imel.kein.email@gmail.com>
2025-08-06 15:03:36 +0200

    Initialize Django project structure with basic settings, URLs, views, and static files for meetups app

---
+++ meetups/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    meetups = [
+        {
+            'title': 'A First Meetup',
+            'location': 'New York',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'location': 'Paris',
+            'slug': 'a-second-meetup'
+        }
+    ]
+    
+    return render(request, "meetups/index.html", {
+        'show_meetups': True,
+        'meetups': meetups
+    })
