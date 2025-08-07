1a15d1cb4df4b79c1c5c9f57ef56460c4ccddf51
imeldaputr <imel.kein.email@gmail.com>
2025-08-07 10:49:04 +0200

    Add image field to Meetup model, update views and templates for image handling

--- meetups/models.py
+++ meetups/models.py
@@ -7,3 +7,4 @@ class Meetup(models.Model):
     location = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='images')
