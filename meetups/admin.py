89d08829f7925a19cd341409407aa4d170190ea9
imeldaputr <imel.kein.email@gmail.com>
2025-08-13 09:43:38 +0200

    Refactor code structure for improved readability and maintainability

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location, Participant
 
 # Register your models here.
 
-admin.site.register(Meetup)
+# How the Meetup model will be displayed in the admin interface
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location')
+    list_filter = ('location', 'date')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -2,9 +2,32 @@ from django.db import models
 
 # Create your models here.
 
+# 1 to N with Meetup
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
+# M to N with Meetup
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.email
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
-    location = models.CharField(max_length=100)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
+    
+    def __str__ (self):
+        return f'{self.title} - {self.slug}'
